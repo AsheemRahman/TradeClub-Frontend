264699c015914b5fa3fa74c6368e52cfa9d96b9a
Asheem Rahman <mhdasheem@gmail.com>
2025-10-25 04:30:55 +0530

    Fix : Register api issue

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -6,22 +6,18 @@ import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import { IRegister, RegisterPayload } from "@/types/types";
 import { useAuthStore } from "@/store/authStore";
-import { useExpertStore } from "@/store/expertStore";
 import { useEffect } from "react";
 
 const UserRegister = () => {
     const router = useRouter();
     const { user, token: userToken } = useAuthStore();
-    const { expert, token: expertToken } = useExpertStore();
 
     // Redirect logic
     useEffect(() => {
         if (user && userToken) {
             router.replace("/home");
-        } else if (expert && expertToken) {
-            router.replace("/expert/dashboard");
         }
-    }, [user, userToken, expert, expertToken, router]);
+    }, [user, userToken, router]);
 
     const handleRegister = async (formData: IRegister) => {
         try {
--- src/app/expert/register/page.tsx
+++ src/app/expert/register/page.tsx
@@ -1,9 +1,38 @@
+import { registerPost } from "@/app/service/shared/sharedApi";
 import Register from "@/components/shared/register";
+import { useExpertStore } from "@/store/expertStore";
+import { IRegister, RegisterPayload } from "@/types/types";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 
 const ExpertRegister = () => {
+    const router = useRouter();
+    const { expert, token: expertToken } = useExpertStore();
+
+    // Redirect logic
+    useEffect(() => {
+        if (expert && expertToken) {
+            router.replace("/expert/dashboard");
+        }
+    }, [expert, expertToken, router]);
+
+    const handleRegister = async (formData: IRegister) => {
+        try {
+            const payload = { ...formData, role: 'user' };
+            const response = await registerPost(payload as RegisterPayload);
+            if (response?.email) {
+                toast.success(response.message || "Registration successful!");
+                router.replace(`/verify-otp?email=${response.email}&type=register`);
+            }
+        } catch (err) {
+            console.error("Error during user register:", err);
+            toast.error("Something went wrong during registration.");
+        }
+    };
     return <>
-        <Register role='expert' />
+        <Register role='expert' onRegister={handleRegister} />
     </>
 };
 
