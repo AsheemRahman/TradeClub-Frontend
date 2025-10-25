5e3b4f493f7b7e7ea5f00b90475044c18dcc5b36
Asheem Rahman <mhdasheem@gmail.com>
2025-10-25 06:10:28 +0530

    add : Suspense

---
+++ src/app/(auth)/message/chat.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import ChatSidebar from '@/components/shared/chatSidebar';
+import ChatWindow from '@/components/shared/chatWindow';
+import { UserMinimal } from '@/types/types';
+import { useAuthStore } from '@/store/authStore';
+import { useSearchParams } from 'next/navigation';
+import userApi from '@/app/service/user/userApi';
+
+
+const Chat = () => {
+    const { user } = useAuthStore()
+    const userId = user?.id as string;
+    const searchParams = useSearchParams();
+    const expertId = searchParams.get('expertId');
+    const [selectedUser, setSelectedUser] = useState<UserMinimal | null>(null);
+
+    // Fetch expert data when expertId is present in URL
+    useEffect(() => {
+        if (expertId) {
+            const fetchExpert = async () => {
+                try {
+                    const expertResponse = await userApi.getExpertById(expertId);
+                    if (!expertResponse.status) throw new Error('Expert not found');
+                    setSelectedUser(expertResponse.expert);
+                } catch (error) {
+                    console.error('Failed to fetch expert:', error);
+                }
+            };
+            fetchExpert();
+        }
+    }, [expertId]);
+
+    return (
+        <div className="min-h-screen bg-gray-100 mx-5 rounded-lg">
+            <div className="h-screen flex">
+                <ChatSidebar role={"User"} onSelectUser={setSelectedUser} selectedUserId={selectedUser?._id} />
+                <ChatWindow role={"User"} selectedUser={selectedUser} currentUserId={userId} />
+            </div>
+        </div>
+    );
+}
+
+export default Chat;
--- src/app/(auth)/message/page.tsx
+++ src/app/(auth)/message/page.tsx
@@ -1,46 +1,10 @@
-'use client'
-export const dynamic = "force-dynamic";
+import React, { Suspense } from 'react';
+import Chat from './chat';
 
-import React, { useEffect, useState } from 'react';
-import ChatSidebar from '@/components/shared/chatSidebar';
-import ChatWindow from '@/components/shared/chatWindow';
-import { UserMinimal } from '@/types/types';
-import { useAuthStore } from '@/store/authStore';
-import { useSearchParams } from 'next/navigation';
-import userApi from '@/app/service/user/userApi';
-
-
-const Chat = () => {
-    const { user } = useAuthStore()
-    const userId = user?.id as string;
-    const searchParams = useSearchParams();
-    const expertId = searchParams.get('expertId');
-    const [selectedUser, setSelectedUser] = useState<UserMinimal | null>(null);
-
-    // Fetch expert data when expertId is present in URL
-    useEffect(() => {
-        if (expertId) {
-            const fetchExpert = async () => {
-                try {
-                    const expertResponse = await userApi.getExpertById(expertId);
-                    if (!expertResponse.status) throw new Error('Expert not found');
-                    setSelectedUser(expertResponse.expert);
-                } catch (error) {
-                    console.error('Failed to fetch expert:', error);
-                }
-            };
-            fetchExpert();
-        }
-    }, [expertId]);
-
+export default function MessagePage() {
     return (
-        <div className="min-h-screen bg-gray-100 mx-5 rounded-lg">
-            <div className="h-screen flex">
-                <ChatSidebar role={"User"} onSelectUser={setSelectedUser} selectedUserId={selectedUser?._id} />
-                <ChatWindow role={"User"} selectedUser={selectedUser} currentUserId={userId} />
-            </div>
-        </div>
+        <Suspense fallback={<div>Loading chat...</div>}>
+            <Chat />
+        </Suspense>
     );
 }
-
-export default Chat;
